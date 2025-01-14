97f4b0228ce843b6d506be85eb8093ec7c8e26f4
txtran224 <txtran224@gmail.com>
2025-01-14 12:20:13 -0500

    local host running

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -5,8 +5,8 @@ const nextConfig = {
     unoptimized: true,
   },
   // Update this to match your GitHub repository name
-  basePath: '/your-repo-name',
-  assetPrefix: '/your-repo-name',
+  // basePath: '/your-repo-name',
+  // assetPrefix: '/your-repo-name',
 }
 
 module.exports = nextConfig
